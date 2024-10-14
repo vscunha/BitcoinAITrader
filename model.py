6d10cdb514eeeae7ab66d16f256ec2728f4adc57
vscunha <victorscunha@outlook.com>
2024-10-13 21:27:54 -0300

    Run pre-commit and renammed stuff.

--- model.py
+++ model.py
@@ -1,81 +1,86 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv1D, MaxPooling1D, LSTM
-from tensorflow.keras import backend as K
-#tf.config.experimental_run_functions_eagerly(True) # used for debuging and development
-tf.compat.v1.disable_eager_execution() # usually using this for fastest performance
+from keras.src import backend
+from keras.src.layers import LSTM, Conv1D, Dense, Flatten, Input, MaxPooling1D
+from keras.src.models import Model
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if len(gpus) > 0:
-    print(f'GPUs {gpus}')
-    try: tf.config.experimental.set_memory_growth(gpus[0], True)
-    except RuntimeError: pass
 
-class Shared_Model:
-    def __init__(self, input_shape, action_space, lr, optimizer, model="Dense"):
-        X_input = Input(input_shape)
+class SharedModel:
+    def __init__(self, input_shape, action_space, learning_rate, optimizer, model='Dense'):
+        x_input = Input(input_shape)
         self.action_space = action_space
 
         # Shared CNN layers:
-        if model=="CNN":
-            X = Conv1D(filters=64, kernel_size=6, padding="same", activation="tanh")(X_input)
-            X = MaxPooling1D(pool_size=2)(X)
-            X = Conv1D(filters=32, kernel_size=3, padding="same", activation="tanh")(X)
-            X = MaxPooling1D(pool_size=2)(X)
-            X = Flatten()(X)
+        if model == 'CNN':
+            x = Conv1D(filters=64, kernel_size=6, padding='same', activation='tanh')(
+                x_input
+            )
+            x = MaxPooling1D(pool_size=2)(x)
+            x = Conv1D(filters=32, kernel_size=3, padding='same', activation='tanh')(x)
+            x = MaxPooling1D(pool_size=2)(x)
+            x = Flatten()(x)
 
         # Shared LSTM layers:
-        elif model=="LSTM":
-            X = LSTM(512, return_sequences=True)(X_input)
-            X = LSTM(256)(X)
+        elif model == 'LSTM':
+            x = LSTM(512, return_sequences=True)(x_input)
+            x = LSTM(256)(x)
 
         # Shared Dense layers:
         else:
-            X = Flatten()(X_input)
-            X = Dense(512, activation="relu")(X)
-        
-        # Critic model
-        V = Dense(512, activation="relu")(X)
-        V = Dense(256, activation="relu")(V)
-        V = Dense(64, activation="relu")(V)
-        value = Dense(1, activation=None)(V)
+            x = Flatten()(x_input)
+            x = Dense(512, activation='relu')(x)
 
-        self.Critic = Model(inputs=X_input, outputs = value)
-        self.Critic.compile(loss=self.critic_PPO2_loss, optimizer=optimizer(lr=lr))
+        # Critic model
+        v = Dense(512, activation='relu')(x)
+        v = Dense(256, activation='relu')(v)
+        v = Dense(64, activation='relu')(v)
+        value = Dense(1, activation=None)(v)
+
+        self.Critic = Model(inputs=x_input, outputs=value)
+        self.Critic.compile(
+            loss=self.critic_ppo2_loss, optimizer=optimizer(learning_rate=learning_rate)
+        )
 
         # Actor model
-        A = Dense(512, activation="relu")(X)
-        A = Dense(256, activation="relu")(A)
-        A = Dense(64, activation="relu")(A)
-        output = Dense(self.action_space, activation="softmax")(A)
+        a = Dense(512, activation='relu')(x)
+        a = Dense(256, activation='relu')(a)
+        a = Dense(64, activation='relu')(a)
+        output = Dense(self.action_space, activation='softmax')(a)
 
-        self.Actor = Model(inputs = X_input, outputs = output)
-        self.Actor.compile(loss=self.ppo_loss, optimizer=optimizer(lr=lr))
-        #print(self.Actor.summary())
+        self.Actor = Model(inputs=x_input, outputs=output)
+        self.Actor.compile(loss=self.ppo_loss, optimizer=optimizer(learning_rate=learning_rate))
+        # print(self.Actor.summary())
 
     def ppo_loss(self, y_true, y_pred):
         # Defined in https://arxiv.org/abs/1707.06347
-        advantages, prediction_picks, actions = y_true[:, :1], y_true[:, 1:1+self.action_space], y_true[:, 1+self.action_space:]
-        LOSS_CLIPPING = 0.2
-        ENTROPY_LOSS = 0.001
-        
+        advantages, prediction_picks, actions = (
+            y_true[:, :1],
+            y_true[:, 1 : 1 + self.action_space],
+            y_true[:, 1 + self.action_space :],
+        )
+        loss_clipping = 0.2
+        entropy_loss = 0.001
+
         prob = actions * y_pred
         old_prob = actions * prediction_picks
 
-        prob = K.clip(prob, 1e-10, 1.0)
-        old_prob = K.clip(old_prob, 1e-10, 1.0)
+        prob = backend.clip(prob, 1e-10, 1.0)
+        old_prob = backend.clip(old_prob, 1e-10, 1.0)
 
-        ratio = K.exp(K.log(prob) - K.log(old_prob))
-        
+        ratio = backend.exp(backend.log(prob) - backend.log(old_prob))
+
         p1 = ratio * advantages
-        p2 = K.clip(ratio, min_value=1 - LOSS_CLIPPING, max_value=1 + LOSS_CLIPPING) * advantages
+        p2 = (
+            backend.clip(
+                ratio, min_value=1 - loss_clipping, max_value=1 + loss_clipping
+            )
+            * advantages
+        )
 
-        actor_loss = -K.mean(K.minimum(p1, p2))
+        actor_loss = -backend.mean(backend.minimum(p1, p2))
 
-        entropy = -(y_pred * K.log(y_pred + 1e-10))
-        entropy = ENTROPY_LOSS * K.mean(entropy)
-        
+        entropy = -(y_pred * backend.log(y_pred + 1e-10))
+        entropy = entropy_loss * backend.mean(entropy)
+
         total_loss = actor_loss - entropy
 
         return total_loss
@@ -83,51 +88,60 @@ class Shared_Model:
     def actor_predict(self, state):
         return self.Actor.predict(state)
 
-    def critic_PPO2_loss(self, y_true, y_pred):
-        value_loss = K.mean((y_true - y_pred) ** 2) # standard PPO loss
+    def critic_ppo2_loss(self, y_true, y_pred):
+        value_loss = backend.mean((y_true - y_pred) ** 2)  # standard PPO loss
         return value_loss
 
     def critic_predict(self, state):
         return self.Critic.predict([state, np.zeros((state.shape[0], 1))])
 
-        
-class Actor_Model:
-    def __init__(self, input_shape, action_space, lr, optimizer):
-        X_input = Input(input_shape)
+
+class ActorModel:
+    def __init__(self, input_shape, action_space, learning_rate, optimizer):
+        x_input = Input(input_shape)
         self.action_space = action_space
 
-        X = Flatten(input_shape=input_shape)(X_input)
-        X = Dense(512, activation="relu")(X)
-        X = Dense(256, activation="relu")(X)
-        X = Dense(64, activation="relu")(X)
-        output = Dense(self.action_space, activation="softmax")(X)
+        x = Flatten(input_shape=input_shape)(x_input)
+        x = Dense(512, activation='relu')(x)
+        x = Dense(256, activation='relu')(x)
+        x = Dense(64, activation='relu')(x)
+        output = Dense(self.action_space, activation='softmax')(x)
 
-        self.Actor = Model(inputs = X_input, outputs = output)
-        self.Actor.compile(loss=self.ppo_loss, optimizer=optimizer(lr=lr))
-        #print(self.Actor.summary)
+        self.Actor = Model(inputs=x_input, outputs=output)
+        self.Actor.compile(loss=self.ppo_loss, optimizer=optimizer(learning_rate=learning_rate))
+        # print(self.Actor.summary)
 
     def ppo_loss(self, y_true, y_pred):
         # Defined in https://arxiv.org/abs/1707.06347
-        advantages, prediction_picks, actions = y_true[:, :1], y_true[:, 1:1+self.action_space], y_true[:, 1+self.action_space:]
-        LOSS_CLIPPING = 0.2
-        ENTROPY_LOSS = 0.001
-        
+        advantages, prediction_picks, actions = (
+            y_true[:, :1],
+            y_true[:, 1 : 1 + self.action_space],
+            y_true[:, 1 + self.action_space :],
+        )
+        loss_clipping = 0.2
+        entropy_loss = 0.001
+
         prob = actions * y_pred
         old_prob = actions * prediction_picks
 
-        prob = K.clip(prob, 1e-10, 1.0)
-        old_prob = K.clip(old_prob, 1e-10, 1.0)
+        prob = backend.clip(prob, 1e-10, 1.0)
+        old_prob = backend.clip(old_prob, 1e-10, 1.0)
 
-        ratio = K.exp(K.log(prob) - K.log(old_prob))
-        
+        ratio = backend.exp(backend.log(prob) - backend.log(old_prob))
+
         p1 = ratio * advantages
-        p2 = K.clip(ratio, min_value=1 - LOSS_CLIPPING, max_value=1 + LOSS_CLIPPING) * advantages
+        p2 = (
+            backend.clip(
+                ratio, min_value=1 - loss_clipping, max_value=1 + loss_clipping
+            )
+            * advantages
+        )
 
-        actor_loss = -K.mean(K.minimum(p1, p2))
+        actor_loss = -backend.mean(backend.minimum(p1, p2))
 
-        entropy = -(y_pred * K.log(y_pred + 1e-10))
-        entropy = ENTROPY_LOSS * K.mean(entropy)
-        
+        entropy = -(y_pred * backend.log(y_pred + 1e-10))
+        entropy = entropy_loss * backend.mean(entropy)
+
         total_loss = actor_loss - entropy
 
         return total_loss
@@ -135,21 +149,24 @@ class Actor_Model:
     def actor_predict(self, state):
         return self.Actor.predict(state)
 
-class Critic_Model:
-    def __init__(self, input_shape, action_space, lr, optimizer):
-        X_input = Input(input_shape)
 
-        V = Flatten(input_shape=input_shape)(X_input)
-        V = Dense(512, activation="relu")(V)
-        V = Dense(256, activation="relu")(V)
-        V = Dense(64, activation="relu")(V)
-        value = Dense(1, activation=None)(V)
+class CriticModel:
+    def __init__(self, input_shape, action_space, learning_rate, optimizer):
+        x_input = Input(input_shape)
 
-        self.Critic = Model(inputs=X_input, outputs = value)
-        self.Critic.compile(loss=self.critic_PPO2_loss, optimizer=optimizer(lr=lr))
+        v = Flatten(input_shape=input_shape)(x_input)
+        v = Dense(512, activation='relu')(v)
+        v = Dense(256, activation='relu')(v)
+        v = Dense(64, activation='relu')(v)
+        value = Dense(1, activation=None)(v)
 
-    def critic_PPO2_loss(self, y_true, y_pred):
-        value_loss = K.mean((y_true - y_pred) ** 2) # standard PPO loss
+        self.Critic = Model(inputs=x_input, outputs=value)
+        self.Critic.compile(
+            loss=self.critic_ppo2_loss, optimizer=optimizer(learning_rate=learning_rate)
+        )
+
+    def critic_ppo2_loss(self, y_true, y_pred):
+        value_loss = backend.mean((y_true - y_pred) ** 2)  # standard PPO loss
         return value_loss
 
     def critic_predict(self, state):
--- utils.py
+++ utils.py
@@ -1,173 +1,208 @@
-import pandas as pd
-from collections import deque
-import matplotlib.pyplot as plt
-from mplfinance.original_flavor import candlestick_ohlc
-import matplotlib.dates as mpl_dates
-from datetime import datetime
 import os
-import cv2
-import numpy as np
+from collections import deque
+from datetime import datetime
 
-def Write_to_file(Date, net_worth, filename='{}.txt'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))):
-    for i in net_worth: 
-        Date += " {}".format(i)
-    #print(Date)
+import cv2
+import matplotlib.dates as mpl_dates
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from mplfinance.original_flavor import candlestick_ohlc
+
+
+def write_to_file(
+    date,
+    net_worth,
+    filename='{}.txt'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+):
+    for i in net_worth:
+        date += ' {}'.format(i)
+    # print(Date)
     if not os.path.exists('logs'):
         os.makedirs('logs')
-    file = open("logs/"+filename, 'a+')
-    file.write(Date+"\n")
+    file = open('logs/' + filename, 'a+')
+    file.write(date + '\n')
     file.close()
+
 
 class TradingGraph:
     # A crypto trading visualization using matplotlib made to render custom prices which come in following way:
     # Date, Open, High, Low, Close, Volume, net_worth, trades
     # call render every step
-    def __init__(self, Render_range, Show_reward=False, Show_indicators=False):
-        self.Volume = deque(maxlen=Render_range)
-        self.net_worth = deque(maxlen=Render_range)
-        self.render_data = deque(maxlen=Render_range)
-        self.Render_range = Render_range
-        self.Show_reward = Show_reward
-        self.Show_indicators = Show_indicators
+    def __init__(self, render_range, show_reward=False, show_indicators=False):
+        self.Volume = deque(maxlen=render_range)
+        self.net_worth = deque(maxlen=render_range)
+        self.render_data = deque(maxlen=render_range)
+        self.render_range = render_range
+        self.show_reward = show_reward
+        self.show_indicators = show_indicators
 
         # We are using the style ‘ggplot’
         plt.style.use('ggplot')
         # close all plots if there are open
         plt.close('all')
         # figsize attribute allows us to specify the width and height of a figure in unit inches
-        self.fig = plt.figure(figsize=(16,8)) 
+        self.fig = plt.figure(figsize=(16, 8))
 
         # Create top subplot for price axis
-        self.ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-        
+        self.ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+
         # Create bottom subplot for volume which shares its x-axis
-        self.ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=self.ax1)
-        
+        self.ax2 = plt.subplot2grid(
+            (6, 1), (5, 0), rowspan=1, colspan=1, sharex=self.ax1
+        )
+
         # Create a new axis for net worth which shares its x-axis with price
         self.ax3 = self.ax1.twinx()
 
         # Formatting Date
         self.date_format = mpl_dates.DateFormatter('%d-%m-%Y')
-        
+
         # Add paddings to make graph easier to view
-        #plt.subplots_adjust(left=0.07, bottom=-0.1, right=0.93, top=0.97, wspace=0, hspace=0)
+        # plt.subplots_adjust(left=0.07, bottom=-0.1, right=0.93, top=0.97, wspace=0, hspace=0)
 
         # define if show indicators
-        if self.Show_indicators:
-            self.Create_indicators_lists()
+        if self.show_indicators:
+            self.create_indicators_lists()
 
-    def Create_indicators_lists(self):
+    def create_indicators_lists(self):
         # Create a new axis for indicatorswhich shares its x-axis with volume
         self.ax4 = self.ax2.twinx()
-        
-        self.sma7 = deque(maxlen=self.Render_range)
-        self.sma25 = deque(maxlen=self.Render_range)
-        self.sma99 = deque(maxlen=self.Render_range)
-        
-        self.bb_bbm = deque(maxlen=self.Render_range)
-        self.bb_bbh = deque(maxlen=self.Render_range)
-        self.bb_bbl = deque(maxlen=self.Render_range)
-        
-        self.psar = deque(maxlen=self.Render_range)
 
-        self.MACD = deque(maxlen=self.Render_range)
-        self.RSI = deque(maxlen=self.Render_range)
+        self.sma7 = deque(maxlen=self.render_range)
+        self.sma25 = deque(maxlen=self.render_range)
+        self.sma99 = deque(maxlen=self.render_range)
 
+        self.bb_bbm = deque(maxlen=self.render_range)
+        self.bb_bbh = deque(maxlen=self.render_range)
+        self.bb_bbl = deque(maxlen=self.render_range)
 
-    def Plot_indicators(self, df, Date_Render_range):
-        self.sma7.append(df["sma7"])
-        self.sma25.append(df["sma25"])
-        self.sma99.append(df["sma99"])
+        self.psar = deque(maxlen=self.render_range)
 
-        self.bb_bbm.append(df["bb_bbm"])
-        self.bb_bbh.append(df["bb_bbh"])
-        self.bb_bbl.append(df["bb_bbl"])
+        self.MACD = deque(maxlen=self.render_range)
+        self.RSI = deque(maxlen=self.render_range)
 
-        self.psar.append(df["psar"])
+    def plot_indicators(self, df, date_render_range):
+        self.sma7.append(df['sma7'])
+        self.sma25.append(df['sma25'])
+        self.sma99.append(df['sma99'])
 
-        self.MACD.append(df["MACD"])
-        self.RSI.append(df["RSI"])
+        self.bb_bbm.append(df['bb_bbm'])
+        self.bb_bbh.append(df['bb_bbh'])
+        self.bb_bbl.append(df['bb_bbl'])
+
+        self.psar.append(df['psar'])
+
+        self.MACD.append(df['MACD'])
+        self.RSI.append(df['RSI'])
 
         # Add Simple Moving Average
-        self.ax1.plot(Date_Render_range, self.sma7,'-')
-        self.ax1.plot(Date_Render_range, self.sma25,'-')
-        self.ax1.plot(Date_Render_range, self.sma99,'-')
+        self.ax1.plot(date_render_range, self.sma7, '-')
+        self.ax1.plot(date_render_range, self.sma25, '-')
+        self.ax1.plot(date_render_range, self.sma99, '-')
 
         # Add Bollinger Bands
-        self.ax1.plot(Date_Render_range, self.bb_bbm,'-')
-        self.ax1.plot(Date_Render_range, self.bb_bbh,'-')
-        self.ax1.plot(Date_Render_range, self.bb_bbl,'-')
+        self.ax1.plot(date_render_range, self.bb_bbm, '-')
+        self.ax1.plot(date_render_range, self.bb_bbh, '-')
+        self.ax1.plot(date_render_range, self.bb_bbl, '-')
 
         # Add Parabolic Stop and Reverse
-        self.ax1.plot(Date_Render_range, self.psar,'.')
+        self.ax1.plot(date_render_range, self.psar, '.')
 
         self.ax4.clear()
         # # Add Moving Average Convergence Divergence
-        self.ax4.plot(Date_Render_range, self.MACD,'r-')
+        self.ax4.plot(date_render_range, self.MACD, 'r-')
 
         # # Add Relative Strength Index
-        self.ax4.plot(Date_Render_range, self.RSI,'g-')
+        self.ax4.plot(date_render_range, self.RSI, 'g-')
 
     # Render the environment to the screen
-    #def render(self, Date, Open, High, Low, Close, Volume, net_worth, trades):
+    # def render(self, Date, Open, High, Low, Close, Volume, net_worth, trades):
     def render(self, df, net_worth, trades):
-        Date = df["Date"]
-        Open = df["Open"]
-        High = df["High"]
-        Low = df["Low"]
-        Close = df["Close"]
-        Volume = df["Volume"]
+        date = df['Date']
+        open = df['Open']
+        high = df['High']
+        low = df['Low']
+        close = df['Close']
+        volume = df['Volume']
         # append volume and net_worth to deque list
-        self.Volume.append(Volume)
+        self.Volume.append(volume)
         self.net_worth.append(net_worth)
 
         # before appending to deque list, need to convert Date to special format
-        Date = mpl_dates.date2num([pd.to_datetime(Date)])[0]
-        self.render_data.append([Date, Open, High, Low, Close])
-        
+        date = mpl_dates.date2num([pd.to_datetime(date)])[0]
+        self.render_data.append([date, open, high, low, close])
+
         # Clear the frame rendered last step
         self.ax1.clear()
-        candlestick_ohlc(self.ax1, self.render_data, width=0.8/24, colorup='green', colordown='red', alpha=0.8)
+        candlestick_ohlc(
+            self.ax1,
+            self.render_data,
+            width=0.8 / 24,
+            colorup='green',
+            colordown='red',
+            alpha=0.8,
+        )
 
         # Put all dates to one list and fill ax2 sublot with volume
-        Date_Render_range = [i[0] for i in self.render_data]
+        date_render_range = [i[0] for i in self.render_data]
         self.ax2.clear()
-        self.ax2.fill_between(Date_Render_range, self.Volume, 0)
+        self.ax2.fill_between(date_render_range, self.Volume, 0)
 
-        if self.Show_indicators:
-            self.Plot_indicators(df, Date_Render_range)
+        if self.show_indicators:
+            self.plot_indicators(df, date_render_range)
 
         # draw our net_worth graph on ax3 (shared with ax1) subplot
         self.ax3.clear()
-        self.ax3.plot(Date_Render_range, self.net_worth, color="blue")
-        
+        self.ax3.plot(date_render_range, self.net_worth, color='blue')
+
         # beautify the x-labels (Our Date format)
         self.ax1.xaxis.set_major_formatter(self.date_format)
         self.fig.autofmt_xdate()
 
-        minimum = np.min(np.array(self.render_data)[:,1:])
-        maximum = np.max(np.array(self.render_data)[:,1:])
-        RANGE = maximum - minimum
-
+        minimum = np.min(np.array(self.render_data)[:, 1:])
+        maximum = np.max(np.array(self.render_data)[:, 1:])
+        range = maximum - minimum
 
         # sort sell and buy orders, put arrows in appropiate order positions
         for trade in trades:
             trade_date = mpl_dates.date2num([pd.to_datetime(trade['Date'])])[0]
-            if trade_date in Date_Render_range:
+            if trade_date in date_render_range:
                 if trade['type'] == 'buy':
-                    high_low = trade['Low'] - RANGE*0.02
-                    ycoords = trade['Low'] - RANGE*0.08
-                    self.ax1.scatter(trade_date, high_low, c='green', label='green', s = 120, edgecolors='none', marker="^")
+                    high_low = trade['Low'] - range * 0.02
+                    ycoords = trade['Low'] - range * 0.08
+                    self.ax1.scatter(
+                        trade_date,
+                        high_low,
+                        c='green',
+                        label='green',
+                        s=120,
+                        edgecolors='none',
+                        marker='^',
+                    )
                 else:
-                    high_low = trade['High'] + RANGE*0.02
-                    ycoords = trade['High'] + RANGE*0.06
-                    self.ax1.scatter(trade_date, high_low, c='red', label='red', s = 120, edgecolors='none', marker="v")
+                    high_low = trade['High'] + range * 0.02
+                    ycoords = trade['High'] + range * 0.06
+                    self.ax1.scatter(
+                        trade_date,
+                        high_low,
+                        c='red',
+                        label='red',
+                        s=120,
+                        edgecolors='none',
+                        marker='v',
+                    )
 
-                if self.Show_reward:
+                if self.show_reward:
                     try:
-                        self.ax1.annotate('{0:.2f}'.format(trade['Reward']), (trade_date-0.02, high_low), xytext=(trade_date-0.02, ycoords),
-                                                   bbox=dict(boxstyle='round', fc='w', ec='k', lw=1), fontsize="small")
-                    except:
+                        self.ax1.annotate(
+                            '{0:.2f}'.format(trade['Reward']),
+                            (trade_date - 0.02, high_low),
+                            xytext=(trade_date - 0.02, ycoords),
+                            bbox=dict(boxstyle='round', fc='w', ec='k', lw=1),
+                            fontsize='small',
+                        )
+                    except Exception:
                         pass
 
         # we need to set layers every step, because we are clearing subplots every step
@@ -180,9 +215,9 @@ class TradingGraph:
 
         """Display image with matplotlib - interrupting other tasks"""
         # Show the graph without blocking the rest of the program
-        #plt.show(block=False)
+        # plt.show(block=False)
         # Necessary to view frames before they are unrendered
-        #plt.pause(0.001)
+        # plt.pause(0.001)
 
         """Display image with OpenCV - no interruption"""
 
@@ -190,68 +225,70 @@ class TradingGraph:
         self.fig.canvas.draw()
         # convert canvas to image
         img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-        img  = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
-        
+        img = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
+
         # img is rgb, convert to opencv's default bgr
         image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
         # display image with OpenCV or any operation you like
-        cv2.imshow("Bitcoin trading bot",image)
+        cv2.imshow('Bitcoin trading bot', image)
 
-        if cv2.waitKey(25) & 0xFF == ord("q"):
+        if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             return
         else:
             return img
-        
 
-def Plot_OHCL(df):
+
+def plot_ohcl(df):
     df_original = df.copy()
     # necessary convert to datetime
-    df["Date"] = pd.to_datetime(df.Date)
-    df["Date"] = df["Date"].apply(mpl_dates.date2num)
+    df['Date'] = pd.to_datetime(df.Date)
+    df['Date'] = df['Date'].apply(mpl_dates.date2num)
 
     df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
-    
+
     # We are using the style ‘ggplot’
     plt.style.use('ggplot')
-    
+
     # figsize attribute allows us to specify the width and height of a figure in unit inches
-    fig = plt.figure(figsize=(16,8)) 
+    fig = plt.figure(figsize=(16, 8))
 
     # Create top subplot for price axis
-    ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+    ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
 
     # Create bottom subplot for volume which shares its x-axis
-    ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+    ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
 
-    candlestick_ohlc(ax1, df.values, width=0.8/24, colorup='green', colordown='red', alpha=0.8)
+    candlestick_ohlc(
+        ax1, df.values, width=0.8 / 24, colorup='green', colordown='red', alpha=0.8
+    )
     ax1.set_ylabel('Price', fontsize=12)
     plt.xlabel('Date')
     plt.xticks(rotation=45)
 
     # Add Simple Moving Average
-    ax1.plot(df["Date"], df_original['sma7'],'-')
-    ax1.plot(df["Date"], df_original['sma25'],'-')
-    ax1.plot(df["Date"], df_original['sma99'],'-')
+    ax1.plot(df['Date'], df_original['sma7'], '-')
+    ax1.plot(df['Date'], df_original['sma25'], '-')
+    ax1.plot(df['Date'], df_original['sma99'], '-')
 
     # Add Bollinger Bands
-    ax1.plot(df["Date"], df_original['bb_bbm'],'-')
-    ax1.plot(df["Date"], df_original['bb_bbh'],'-')
-    ax1.plot(df["Date"], df_original['bb_bbl'],'-')
+    ax1.plot(df['Date'], df_original['bb_bbm'], '-')
+    ax1.plot(df['Date'], df_original['bb_bbh'], '-')
+    ax1.plot(df['Date'], df_original['bb_bbl'], '-')
 
     # Add Parabolic Stop and Reverse
-    ax1.plot(df["Date"], df_original['psar'],'.')
+    ax1.plot(df['Date'], df_original['psar'], '.')
 
     # # Add Moving Average Convergence Divergence
-    ax2.plot(df["Date"], df_original['MACD'],'-')
+    ax2.plot(df['Date'], df_original['MACD'], '-')
 
     # # Add Relative Strength Index
-    ax2.plot(df["Date"], df_original['RSI'],'-')
+    ax2.plot(df['Date'], df_original['RSI'], '-')
 
     # beautify the x-labels (Our Date format)
-    ax1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%y-%m-%d'))# %H:%M:%S'))
+    ax1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%y-%m-%d'))  # %H:%M:%S'))
     fig.autofmt_xdate()
     fig.tight_layout()
-    
+
     plt.show()
